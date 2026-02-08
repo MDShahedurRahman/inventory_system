a277787e6770ec7e12f832d77799a77e2020c35e
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-07 21:44:46 -0500

    Implement to_dict() method

--- models/customer.py
+++ models/customer.py
@@ -3,3 +3,9 @@ class Customer:
     def __init__(self, customer_id, name):
         self.customer_id = customer_id
         self.name = name
+
+    def to_dict(self):
+        return {
+            "customer_id": self.customer_id,
+            "name": self.name
+        }
