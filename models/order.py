5d53a1238b4ae9dfa7e3ea9ec73bf8b3277db2c5
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-07 21:47:29 -0500

    Implement Order constructor

--- models/order.py
+++ models/order.py
@@ -1,1 +1,8 @@
 class Order:
+
+    def __init__(self, order_id, customer_id, product_id, quantity, total_price):
+        self.order_id = order_id
+        self.customer_id = customer_id
+        self.product_id = product_id
+        self.quantity = quantity
+        self.total_price = total_price
