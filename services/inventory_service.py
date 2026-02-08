36dcadbbbffb4427ec4619f57ca6b87ff824c7df
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-07 21:56:45 -0500

    Implement place_order() method

--- services/inventory_service.py
+++ services/inventory_service.py
@@ -31,3 +31,34 @@ def add_customer(self, customer_data):
     customer = Customer(new_id, customer_data["name"])
     data["customers"].append(customer.to_dict())
     self.repo.save_data(data)
+
+
+def place_order(self, order_data):
+    data = self.repo.load_data()
+    new_id = len(data["orders"]) + 1
+
+    product = next(
+        (p for p in data["products"]
+         if p["product_id"] == order_data["product_id"]),
+        None
+    )
+
+    if not product:
+        print("Product not found.")
+        return
+
+    if product["stock"] < order_data["quantity"]:
+        print("Not enough stock available.")
+        return
+
+    total_price = product["price"] * order_data["quantity"]
+    product["stock"] -= order_data["quantity"]
+
+    order = Order(new_id,
+                  order_data["customer_id"],
+                  order_data["product_id"],
+                  order_data["quantity"],
+                  total_price)
+
+    data["orders"].append(order.to_dict())
+    self.repo.save_data(data)
