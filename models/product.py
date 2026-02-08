660b80228abf6287bc576d5587a6489e07d892d3
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-07 21:37:48 -0500

    Implement product constructor

--- models/product.py
+++ models/product.py
@@ -1,1 +1,7 @@
 class Product:
+
+    def __init__(self, product_id, name, price, stock):
+        self.product_id = product_id
+        self.name = name
+        self.price = price
+        self.stock = stock
