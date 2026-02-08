fc245198270c7fcf4961d2da289b0b6b4b2c8382
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-08 12:44:38 -0500

    Implement  show_menu() method

--- views/inventory_view.py
+++ views/inventory_view.py
@@ -1,1 +1,12 @@
 class InventoryView:
+
+    def show_menu(self):
+        print("\n=== Inventory & Order System ===")
+        print("1. Add Product")
+        print("2. Add Customer")
+        print("3. Place Order")
+        print("4. View Inventory")
+        print("5. View Orders")
+        print("6. Sales Analytics")
+        print("0. Exit")
+        return input("Choose: ")
