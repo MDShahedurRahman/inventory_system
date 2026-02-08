fa1ab86d116cfac77a33d5892b4ccf0a369d5bfa
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-07 21:20:41 -0500

    Implement main entry point

---
+++ main.py
@@ -0,0 +1,10 @@
+from controllers.inventory_controller import InventoryController
+
+
+def main():
+    controller = InventoryController()
+    controller.run()
+
+
+if __name__ == "__main__":
+    main()
